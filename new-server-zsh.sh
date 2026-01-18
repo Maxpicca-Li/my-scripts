a96dc4c3c4dc190dcfcceccd95c4305125de2988
Yanqin Li <maxpicca@qq.com>
2026-01-18 16:04:25 +0800

    clean: comment infrequently used plugin

--- new-server-zsh.sh
+++ new-server-zsh.sh
@@ -27,11 +27,11 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-autosuggestions
 # 语法高亮
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# 高频目录统计和快速跳转
-git clone --depth=1 https://github.com/wting/autojump.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autojump" \
-&& pushd "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autojump" \
-&& ./install.py \
-&& popd
+# 高频目录统计和快速跳转(低频使用)
+# git clone --depth=1 https://github.com/wting/autojump.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autojump" \
+# && pushd "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/autojump" \
+# && ./install.py \
+# && popd
 # autojump install 结束后会提示内容到.zshrc中，照做即可
 
 # 5. 修改 .zshrc
